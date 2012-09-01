0c3a19d5e8b23419542bf4d779d0a24cc6eaf919
wangshengzhong <632618392@qq.com>
2012-09-01 19:26:07 +0800

    更新Session相关

--- test/com/daxia/hibernate/model/StudentTest.java
+++ test/com/daxia/hibernate/model/StudentTest.java
@@ -1,5 +1,6 @@
 package com.daxia.hibernate.model;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -15,12 +16,12 @@ public class StudentTest {
 		sf = new Configuration().configure().buildSessionFactory();
 	}
 	
-	//测试session的save方法
+	//test.session.save()
 	@Test
 	public void testSave(){
 		Student stu = new Student();
-		stu.setAge(55);
-		stu.setName("zhang");
+		stu.setAge(22);
+		stu.setName("hujing");
 		
 		Session s = sf.getCurrentSession();
 		s.beginTransaction();
@@ -28,10 +29,91 @@ public class StudentTest {
 		s.getTransaction().commit();
 	}
 	
-	//测试session的update
+	//test session.delete()
+	@Test
+	public void testDelete(){
+		Student stu = new Student();
+		stu.setName("hujing");
+		stu.setAge(22);
+		Session s = sf.getCurrentSession();
+		s.beginTransaction();
+		s.save(stu);
+		s.getTransaction().commit();
+		
+		//删除数据
+		Session s2 = sf.getCurrentSession();
+		s2.beginTransaction();
+		s2.delete(stu);
+		s2.getTransaction().commit();
+	}
+	
+	//test session.get()
+	@Test
+	public void testGet(){
+		
+		Session s = sf.getCurrentSession();
+		s.beginTransaction();
+		Student stu2 = (Student) s.get(Student.class, 4);
+		System.out.println(stu2.getName());
+		s.getTransaction().commit();
+	}
+	
+	//test session.load()
+	@Test
+	public void testLoad(){
+		
+		Session s = sf.getCurrentSession();
+		s.beginTransaction();
+		//代理 Proxy load()   第二个参数：id
+		Student stu2 = (Student) s.load(Student.class, 4);
+		System.out.println(stu2.getName());
+		s.getTransaction().commit();
+	}
+	
+	
+	//test session.update()   先获取表数据，然后更新需要更新的表数据字段
 	@Test
 	public void testUpdate(){
 		
+		//获取表数据
+		Session s = sf.getCurrentSession();
+		s.beginTransaction();
+		Student stu2 = (Student) s.get(Student.class, 1);
+		s.getTransaction().commit();
+		
+		//更新表字段
+		stu2.setName("zhang");
+		
+		//update
+		Session s2 = sf.getCurrentSession();
+		s2.beginTransaction();
+		s2.update(stu2);
+		s2.getTransaction().commit();
+	}
+	
+	//update 至少需要知道id
+	@Test
+	public void testUpdate2(){
+		Student stu = new Student();
+		stu.setId(1);
+		
+		stu.setName("wang");
+		Session s = sf.getCurrentSession();
+		s.beginTransaction();
+		s.update(stu);
+		s.getTransaction().commit();
+		
+	}
+	
+	//只更新部分字段
+	@Test
+	public void testUpdate4(){
+		Session s = sf.getCurrentSession();
+		s.beginTransaction();
+		//HQL:Student是对象，非表
+		Query q = s.createQuery("update Student s set s.name='hu' where s.id=1");
+		q.executeUpdate();
+		s.getTransaction().commit();
 	}
 	@AfterClass
 	public static void afterClass(){
